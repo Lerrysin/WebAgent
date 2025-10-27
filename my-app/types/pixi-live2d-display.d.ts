009d5fa5afa24d26110d50992ccbd956c9008e1f
Lerrysin <adversmin@outlook.com>
2025-10-27 19:44:36 +0800

    chore: remove redundant files

--- my-app/types/pixi-live2d-display.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "pixi-live2d-display/cubism4" {
-interface Live2DModel {
-setParameterValueById(id: string, value: number): void;
-getParameterValueById(id: string): number;
-addParameterValueById(id: string, value: number, weight?: number): void;
-}
-}
